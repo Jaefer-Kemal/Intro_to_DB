d4d8757eff998437c2e8596b407417091fb6c8a0
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-28 22:28:32 +0300

    Changed the exception into more specific keyword - mysql.connector.Error

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,7 +13,7 @@ mycursor = mydb.cursor()
 try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
-except Exception as e:
+except mysql.connector.Error as e:
     print(e)
     
 mycursor.close()
