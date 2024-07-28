94e82edcf7066c6ce80dc801c01d7f5aa62d2fc2
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-28 22:26:25 +0300

    Added new file MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="test",
+    auth_plugin='mysql_native_password'
+)
+
+mycursor = mydb.cursor()
+
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except Exception as e:
+    print(e)
+    
+mycursor.close()
+mydb.close()
