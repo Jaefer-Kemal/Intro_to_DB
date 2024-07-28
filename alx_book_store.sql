09b679082676f14bc40ca054eb508a5c9a53d64d
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-28 22:15:54 +0300

    Fixed spacing to adhere to ALX standard

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,15 +26,15 @@ CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
 
